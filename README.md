8da0fa03d1921da86ab66bacb352a0b9e485d2ad
Milos Vetesnik <milos@excalidraw.com>
2025-01-26 21:14:20 +0100

    feat: added railway very basic config

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 ```
 docker run --name postgres -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=postgres -p 127.0.0.1:5432:5432 -d postgres
 ```
+
+## Production:
+
+- vercel + railway
+
+Followed these steps: https://docs.medusajs.com/resources/deployment/medusa-application/railway
--- bonybons/medusa-config.ts
+++ bonybons/medusa-config.ts
@@ -2,6 +2,8 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
+
+// https://docs.medusajs.com/resources/deployment/medusa-application/railway
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
@@ -11,6 +13,36 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+    },
+
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
+    redisUrl: process.env.REDIS_URL,
+
+  },
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+  },
+
+  modules: [
+    {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
+      },
+    },
+  ],
 })
